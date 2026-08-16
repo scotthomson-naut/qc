5d1f70d23e03bf1db222ee21cedca39e110139e7
scotthomson-naut <scot.thomson@gmail.com>
2026-08-16 13:31:28 -0400

    Changed invert icon again

--- qc_checker/scriptronaut_qc/ui/panel.py
+++ qc_checker/scriptronaut_qc/ui/panel.py
@@ -377,7 +377,7 @@ class SCRIPTRONAUT_PT_QC_Checks(Panel):
 
         row.operator(
             "scriptronaut.qc_select_invert",
-            icon="SELECT_DIFFERENCE",
+            icon="CLIPUV_HLT",
             text="",
         )
 
