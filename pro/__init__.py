e1b9a6271f15b00b58f70f5fface74f03aa6bf32
scotthomson-naut <scot.thomson@gmail.com>
2026-09-03 16:19:41 -0400

    Added missing data

--- pro/__init__.py
+++ pro/__init__.py
@@ -1,5 +1,15 @@
 """Scriptronaut QC Checker Pro Blender Extension entry point."""
 
+bl_info = {
+    "name": "Scriptronaut QC Checker Pro",
+    "author": "Scriptronaut",
+    "version": (1, 0, 0),
+    "blender": (4, 3, 0),
+    "location": "3D View > Sidebar > Scriptronaut",
+    "description": "Production-focused scene and asset QC checks for Blender.",
+    "category": "3D View",
+}
+
 from .scriptronaut_qc.registration import (
     register as register_core,
     unregister as unregister_core,
