48c2f13a60df3004ba59c2b191e145eb26a1abc6
scotthomson-naut <scot.thomson@gmail.com>
2026-09-08 13:26:44 -0400

    Removed wrod private User  change to a name

--- documentation/template/beta-feedback-config.example.php
+++ documentation/template/beta-feedback-config.example.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
  * Copy this file to beta-feedback-config.php on the web server.
  * Keep the real config out of source control and do not make it downloadable.
  * Generate each hash with:
- *    'recipient' => 'beta@scriptronaut.com',
  *     php -r "echo password_hash('TESTER ACCESS CODE', PASSWORD_DEFAULT), PHP_EOL;"
+ *    'recipient' => 'beta@scriptronaut.com',
+ *            // 'tester-id' => '$2y$10$REPLACE_WITH_A_REAL_PASSWORD_HASH',
 */
 return [
     'recipient' => 'scot.thomson@gmail.com',
@@ -17,6 +18,6 @@ return [
     'rate_limit_count' => 12,
     'rate_limit_window_seconds' => 3600,
     'testers' => [
-        // 'tester-id' => '$2y$10$REPLACE_WITH_A_REAL_PASSWORD_HASH',
+        'Scot' => '$2y$12$sQAyQvw5p1nKpQ1IfKLFp.5a8iWaREOGOs93IPLWJUtvYOwUCQyOK',
     ],
 ];
